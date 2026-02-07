24274b356a1e0d7b141a875b299aad9fb7ba2fb1
Ian Thompson <ianiat11@gmail.com>
2026-02-07 11:57:38 -0600

    Added `category` to :func:`main`.
    This allows the "player 1" to submit a category as input and then prints
    it to the screen so "player 2" can see it.
    
    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's play hangman!");
+    println!("Player 1, give me a category:");
+    let mut category = String::new();
+    io::stdin()
+        .read_line(&mut category)
+        .expect("Failed to read line");
+    println!("The category is \"{category}\"");
 }
