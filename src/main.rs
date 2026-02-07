f380a8bc0bb59384d9673b02aea33b0bdd31f447
Ian Thompson <ianiat11@gmail.com>
2026-02-07 12:18:52 -0600

    Defined `word` in :func:`main`.
    This asks "player 1" for a word in the provided category. It will be the
    answer that "player 2" must figure out.
    
    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,11 @@ fn main() {
     // Trime whitespace from `category`.
     let category = category.trim();
     println!("The category is \"{category}\"");
+    println!("Player 1, give me a word in the category:");
+    let mut word = String::new();
+    io::stdin()
+        .read_line(&mut word)
+        .expect("Failed to read line");
+    let word = word.trim();
+    println!("The word is \"{word}\"");
 }
