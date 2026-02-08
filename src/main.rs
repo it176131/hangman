4042b512a742cd5ee4712fa1c1dc1cd9e9c16c6c
Ian Thompson <ianiat11@gmail.com>
2026-02-08 12:56:55 -0600

    Added guessing loop to :func:`main`.
    This allows "player 2" to guess until they run out of incorrect guesses
    (6).
    
    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,26 @@ fn main() {
     println!("The word is \"{word}\"");
     let len = word.chars().count();
     println!("There are {len} letters in the word.");
-    let mut incorrect: u8 = 0;
-    println!("Player 2, guess a letter:");
-    let mut letter = String::new();
-    io::stdin()
-        .read_line(&mut letter)
-        .expect("Failed to read line");
-    let letter = letter.trim();
-    println!("The letter is \"{letter}\"");
-    match word.contains(letter) {
-        true => println!("The word contains the letter {letter}."),
-        false => {
-            println!("The word does _not_ contain the letter {letter}.");
-            incorrect += 1;
-            println!("Total incorrect: {incorrect}");
-        },
+    let mut incorrect = 0;
+    loop {
+        println!("Player 2, guess a letter:");
+        let mut letter = String::new();
+        io::stdin()
+            .read_line(&mut letter)
+            .expect("Failed to read line");
+        let letter = letter.trim();
+        println!("The letter is \"{letter}\"");
+        match word.contains(letter) {
+            true => println!("The word contains the letter {letter}."),
+            false => {
+                println!("The word does _not_ contain the letter {letter}.");
+                incorrect += 1;
+                println!("Total incorrect: {incorrect}");
+            }
+        }
+        if incorrect == 6 {  // head + torso + 2 arms + 2 legs = 6
+            println!("You lose.");
+            break
+        }
     }
 }
